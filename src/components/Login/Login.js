42eb45c1754aedabfec9d9b2cdac6e4881a998b1
AnngelRuiz <anngelruiz@hotmail.com>
2024-05-21 14:56:25 -0600

    Change Login design

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,75 +1,49 @@
 import React, { useState } from'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import logoWhiteImg from '../../images/logo-white.svg';
 import logingImg from '../../images/login-bg.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookSquare, faGoogle, faSquareTwitter } from '@fortawesome/free-brands-svg-icons';
+// import { faUser, faLock } from '@fortawesome/free-core-svg-icons';
+// import { faUser, faLock } from '@fortawesome/fontawesome-svg-core';
+import { faUser, faLock } from '@fortawesome/free-solid-svg-icons'
+
 import './login.css';
 
 const Login = () => {
 
-    const[email, setEmail] = useState('');
-    const[password, setPassword] = useState('');
-    
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value);
-    };
-
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
-    };
-
-    const handleLogin = async () => {
-        try{
-            const response = await axios.post('http://localhost:8080/login', {
-                email,
-                password
-            });
-
-            if(response.status === 200){
-                const data = response.data;
-
-                localStorage.setItem('token', data.token);
-            }
-        }catch(error){
-            console.error('Error during login:', error);
-        }
-    }
-
     return (
-        <>
-            <div className="container login">
-                <div className="forms-container">
-                    <div className="signin-signup">
-                        <form action="" className="formBase sign-in-form">
-                            <h2 className="title">Login</h2>
-                            <div className="input-field">
-                                <i className="fas fa-user"></i>
-                                <input type="text" placeholder="Username"/>
-                            </div>
-                            <div className="input-field">
-                                <i className="fas fa-lock"></i>
-                                <input type="password" placeholder="Password"/>
-                            </div>
-                                                                                    
-                            <Link className="btn solid" to="/dashboard">Login</Link>
-                        </form>
-
-                    </div>
+        <div className='containerCenter'>                        
+            <div className='contiainerLogin'>
+                <div class="leftPanel">
+                    <img src={logingImg} alt="Campfire"></img>
                 </div>
-
-                <div className="panels-container">
-                    <div className="panel left-panel">
-                        <div className="content">
-                            <div className="right-title"><img src={logoWhiteImg} alt="" className="right-image"/><h3>IKA CLIMBING</h3></div>
-                            <p>GYM Dashboard clients</p>                            
+                <div class="rightPanel">
+                    <h5>LOGIN</h5>
+                    <form>
+                        <div class="input-container">
+                            <FontAwesomeIcon icon={faUser} className="fa"/>
+                            <input type="text" placeholder='Username' />
                         </div>
+                        <div class="input-container">
+                            <FontAwesomeIcon icon={faLock} className="fa"/>
+                            <input type="password" placeholder='Password' />
+                        </div>
+                        <span>Recovery password</span>
+                        <button className='button md primary'>Login</button>
+                    </form>
 
-                        <img src={logingImg} className="image" alt="login bg"/>
+                    <hr></hr>
+                    <p>Or continue with:</p>
+
+                    <div className='loginBrandIcons'>
+                        <div className="brandIcon"><FontAwesomeIcon icon={faFacebookSquare} size="2x" title="Facebook"/></div>
+                        <div className="brandIcon"><FontAwesomeIcon icon={faGoogle} size="2x" title="Google"/></div>
+                        <div className="brandIcon"><FontAwesomeIcon icon={faSquareTwitter} size="2x" title="Twitter"/></div>
                     </div>
-
                 </div>
             </div>
-        </>
+        </div>
     );
 }
 
